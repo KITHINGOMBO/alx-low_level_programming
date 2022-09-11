f685f121a034b3163792330a4674cbb8d5cf10b7
KITHINGOMBO <denniskithi@gmail.com>
2022-09-11 13:48:45 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,10 +3,11 @@
  * main - Entry point
  * Return: 0.
  */
-int main (void)
+int main(void)
 {
 	int num;
 	char letter;
+
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	for (letter = 'a'; letter <= 'f'; letter++)
