47067f664d22f013983411ea410a4efbde6bc2fa
KITHINGOMBO <denniskithi@gmail.com>
2022-10-01 02:10:36 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,11 +3,14 @@
 
 /**
   * main - Prints all the args content
+  *
   * @argc: argument count
+  *
   * @argv: argument vector
   *
   * Return: Always zero
   */
+
 int main(int argc, char *argv[])
 {
 	int n;
