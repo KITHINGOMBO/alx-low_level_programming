a70fbc21f336934059981e898ec888dee4bbc86c
KITHINGOMBO <denniskithi@gmail.com>
2022-10-01 01:59:05 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the number of args
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void) argv;
+	return (0);
+}
